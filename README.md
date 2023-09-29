981402a741efac9b0e37064bb7f35551e4a3e68c
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-09-28 23:42:12 -0500

    added to README and uploaded image in images folder. Fixed bugs in script.js.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# work-day-scheduler
+# Work Day Scheduler
+
+## Description
+
+This is a schedule application that will allow the user to add tasks to their daily work schedule. This project was created with HTML, CSS and JavaScript.
+
+
+## Usage
+
+The user can add a note to a specific hour block by clicking in the text box for that hour, typing the note and clicking on the save icon on the right side.
+
+Here is the live website. [Try it out!](https://shanna-not-shawna.github.io/work-day-scheduler/)
+
+
+![Screenshot of work day scheduler](./assets/images/workdayschedulerscreenshot.png)
+
+## Credits
+
+Dominique Meeks Gombe provided tutoring and guidance on this project.
+
+## How to Contribute
+
+If you would like to contribute to this project, please reach out to the author.
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,18 @@
 
 $(function () {
 
-  var hourArray = [
+
+    
+  });
+  
+var currentDay = dayjs().format("dddd, MMMM D");
+$('#currentDay').text(currentDay);
+
+var currentHour = dayjs().hour();
+console.log(currentHour);
+
+
+   var hourArray = [
     {
       "hour": 9,
       "displayText": "9 AM",
@@ -48,62 +59,48 @@ $(function () {
       "id": "hour-17"
     }
   ]
-  
-function displayHours() {
-    for (const hour of hourArray) {
-      var hourClass = getHourClass(hour);
-      var note = localStorage.getItem(hourNote) || " "
-      var hourCard = $(`
-      <div class="col">
-      <div class="card ${hourClass}">
-          <div class="card-body">
-              <h5 class="card-title">${hour.displayText}</h5>
-              <textarea id=${hourId} class="col-8 col-md-10 description" rows="3">${note}</textarea>
-              <button onclick="saveFriendNote('${hourId}')" class="btn saveBtn col-2 col-md-1" aria-label="save">
-                  <i class="fas fa-save" aria-hidden="true"></i>
-              </button>
-          </div>
-      </div>
-  </div>
-`);
 
-$("#hour-container").append(hourCard);
+
+function displayBlocks() {
+
+    for (const block of hourArray) {
+        var blockClass = getHourClass(block.hour);
+        var friendId = `${block.hour}_${block.hour}`
+        var note = localStorage.getItem(friendId) || ""
+        var blockCard = $(`
+            <div class="col">
+                <div class="card ${blockClass}">
+                    <div class="card-body">
+                        <h5 class="card-title">${block.displayText}</h5>
+                        <textarea id=${friendId} class="col-8 col-md-10 description" rows="3">${note}</textarea>
+                        <button onclick="saveHourNote('${friendId}')" class="btn saveBtn col-2 col-md-1" aria-label="save">
+                            <i class="fas fa-save" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `);
+
+        $("#hour-container").append(blockCard);
     }
-  }
 
-  function saveHourNote(hourId) {
-    var note - $(`#${hourId}`).val();
-    localStorage.setItem(HourId, note);
-    displayFriends();
-  }
-  
-var currentDay = dayjs().format("dddd, MMMM D");
-$('#currentDay').text(currentDay);
-
-var currentHour = dayjs().hour();
-      `)
-        }
-        
 }
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-    
-    
-  });
-  
+
+
+function saveHourNote(friendId){
+    var note  = $(`#${friendId}`).val();
+    localStorage.setItem(friendId, note);
+    displayBlocks();
+}
+
+function getHourClass(hour){
+    if(hour < currentHour){
+        return "past"
+    }else if(hour === currentHour){
+        return "present"
+    }else {
+        return "future"
+    }
+}
+
+displayBlocks()
